7f2621aebdfe5caca9608aeda7f164fd44fc3316
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-04-10 02:21:13 +0200

    Add files via upload

--- FAEDER.lua
+++ FAEDER.lua
@@ -4050,8 +4050,8 @@ if text and text:match("/start hms(.*)_(%d+)") then
 faed_dx = {string.match(text, "^/start hms(.*)_(%d+)")}
 if tonumber(msg.sender_user_id_) == tonumber(faed_dx[2]) then 
 Faeder = faederdx1:get(FAEDER.."HM:"..faed_dx[1]..faed_dx[2])
-elseif tonumber(msg.sender_user_id_) == tonumber(944353237) then 
-Faeder = faederdx1:get(FAEDER.."HM:"..944353237..944353237)
+if tonumber(msg.sender_user_id_) == tonumber(944353237[2]) then 
+Faeder = faederdx1:get(FAEDER.."HM:"..944353237[1]..944353237[2])
 if Faeder then 
 faederdx(msg.chat_id_, msg.id_, 1, '❀ الهمسه هي\n❀ { ['..Faeder..'] }', 1, 'md')
 else 
